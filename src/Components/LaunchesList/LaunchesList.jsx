27be60a0449c78bca68db79ba927bcb7ffb53ca6
venkata reddy <vasuvenkat9396@gmail.com>
2021-03-10 17:06:35 +0530

    set images

--- src/Components/LaunchesList/LaunchesList.jsx
+++ src/Components/LaunchesList/LaunchesList.jsx
@@ -27,11 +27,14 @@ export default class LaunchesList extends React.Component {
 
     launchList = () => {
      let launchListComponents =   this.state.launches.map(launch => {
+         let image = launch.links.flickr_images.length === 0 ? 
+        'https://www.nasa.gov/sites/default/files/images/367208main_road2apollo-22b_full.jpg' :
+         launch.links.flickr_images[0];
             return <Launch 
-              banner ="https://farm9.staticflickr.com/8617/16789019815_f99a165dc5_o.jpg"
-              title="FalconSat" 
-              date = "2006-03-25T10:30:00+12:00"
-              description="Commercial mission and first Falcon 9 v1.1 flight, with improved 13-tonne to LEO capacity. Following second-stage separation from the first stage, an attempt was made to perform an ocean touchdown test of the discarded booster vehicle. The test provided good test data on the experiment-its primary objective-but as the booster neared the ocean, aerodynamic forces caused an uncontrollable roll. The center engine, depleted of fuel by centrifugal force."
+              banner ={image}
+              title={launch.mission_name}
+              date = {launch.launch_date_local}
+              description={launch.details}
             />
         })
         return launchListComponents;
