7d16141cb4d27fbc973249c3724e1c775352f89b
venkata reddy <vasuvenkat9396@gmail.com>
2021-03-12 13:38:16 +0530

    created individual component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import LaunchesList from './Components/LaunchesList/LaunchesList';
+import LaunchView from './Components/LaunchView/LaunchView';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <LaunchesList/>
-    </div>
+
+    <Router>
+        <Header/>
+        <Switch>
+            <Route path="/launch/:flight_number">
+                <LaunchView/>
+            </Route>
+            <Route path="/"> 
+              <LaunchesList/>
+            </Route>
+        </Switch>
+         </Router>
+
+      </div>
+   
   );
 }
 
--- src/Components/LaunchesList/LaunchesList.jsx
+++ src/Components/LaunchesList/LaunchesList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Launch from '../Launch/Launch';
 import './style.css';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class LaunchesList extends React.Component {
 
@@ -32,13 +33,19 @@ export default class LaunchesList extends React.Component {
         'https://www.extremetech.com/wp-content/uploads/2020/06/spacex-crew-launch-nasa.jpg' :
          launch.links.flickr_images[0];
 
-            return <Launch 
-              key = {'launch_'+index}   // to avoid errors in the consoles each child should have specific key
+            return (
+            <Link 
+            key = {'launch_'+index}            // to avoid errors in the consoles each child should have specific key
+            to={'/launch/'+launch.flight_number}
+            >
+            <Launch                                        
               banner ={image}
               title={launch.mission_name}
               date = {launch.launch_date_local}
               description={launch.details}
             />
+            </Link>
+            )
         })
         return launchListComponents;
     }
